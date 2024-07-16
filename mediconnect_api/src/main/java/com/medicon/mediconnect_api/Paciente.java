e53369eb48d68f2389ab95b9bb699476bc8a658a
FloraOFF <floraoliveirafranca@gmail.com>
2024-07-16 15:22:15 -0500

    Paciente model

---
+++ mediconnect_api/src/main/java/com/medicon/mediconnect_api/Paciente.java
@@ -0,0 +1,91 @@
+package com.medicon.mediconnect_api;
+
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Entity
+public class Paciente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, updatable = false)
+    private Long id;
+    @Column(nullable = false)
+    private String nome;
+    @Column(nullable = false)
+    private String email;
+    @Column(nullable = false)
+    private String telefone;
+    @Column(nullable = false)
+    private Date dataNascimento;
+    @Column(nullable = false)
+    private String grupoSanguineo;
+    @Column(nullable = false)
+    private String sexo;
+    @Column(nullable = false)
+    private String cep;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public Date getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(Date dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getGrupoSanguineo() {
+        return grupoSanguineo;
+    }
+
+    public void setGrupoSanguineo(String grupoSanguineo) {
+        this.grupoSanguineo = grupoSanguineo;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+}
